6c86e7c64aa3377e1c765dc587b963112fc52a3f
drewmullen <drmullen@amazon.com>
2022-06-20 12:43:40 -0400

    switch to ssm

--- main.tf
+++ main.tf
@@ -1,12 +1,5 @@
-module "vpc" {
-  source  = "aws-ia/vpc/aws"
-  version = ">= 1.0.0"
-
-  name       = "multi-az-vpc"
-  cidr_block = "10.0.0.0/20"
-  az_count   = 1
-
-  subnets = {
-    private = { netmask = 24 }
-  }
+resource "aws_ssm_parameter" "foo" {
+  name  = var.workspace_name
+  type  = "String"
+  value = "bar"
 }
